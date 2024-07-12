507358bbee369a1b83d69e79c2873d2f7d8125f1
Deltager <teknisk+GDC2024-GR5@game.unf.dk>
2024-07-12 14:49:32 +0200

    Opgave 1 (done)

---
+++ Opgaver/Assets/Vlad.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vlad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hej, jeg er ond");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+    
